8418fa7aa46673ec8a0ef1af20a82331f2c5ccef
Cosme Barbosa <cosmeg5@hotmail.com>
2020-08-13 15:24:42 -0300

    Iniciando aulas com o projeto de calculadora IMC, feito o HTML, CSS e função da calcuadora de IMC

---
+++ script.js
@@ -0,0 +1,33 @@
+let pacientes = document.getElementsByClassName("paciente");
+
+for(let position = 0; position <= pacientes.length - 1; position++) {
+
+    let paciente = pacientes[position];
+
+    let nomeTd = paciente.getElementsByClassName("nomeTd")[0];
+    let pesoTd = paciente.getElementsByClassName("pesoTd")[0];
+    let alturaTd =paciente.getElementsByClassName("alturaTd")[0];
+
+    let infoPaciente = {
+        nome: nomeTd.textContent,
+        peso: parseFloat(pesoTd.textContent),
+        altura: parseFloat(alturaTd.textContent),
+        pegaImc : function() {
+            if((this.altura != 0) && (this.peso != 0)) {
+                let imc = this.peso / (this.altura * this.altura);
+        
+                return imc;
+            }
+        }
+    };
+
+    let imc = infoPaciente.pegaImc();
+
+    let imcTd =paciente.getElementsByClassName("imcTd")[0];
+    imcTd.textContent = imc;
+
+}
+
+
+
+
